fcd6ba9983bf0624d720ef820611f1a658611d2e
Caspar Sylwan <caspar.sylwan@gmail.com>
2018-09-24 07:23:13 +0200

    commit 6

--- R/linereg.R
+++ R/linereg.R
@@ -22,11 +22,11 @@
 #'
 #' @examples
 linreg <- setRefClass("linreg",
-                      fields = list(dep = "numeric", indep = "matrix", betahat = "matrix",
+                      fields = list(dep = "numeric", indep = "matrix", betahat = "numeric",
                                     predicted = "matrix", residuals = "matrix",inv_sqr_dep = "matrix",
-                                    deg_fre = "numeric", sigmasq="numeric", var_betas = "numeric",
-                                    t_values = "numeric",p_values = "numeric" ,data="data.frame",
-                                    formula="formula", dataname="character"),
+                                    deg_fre = "numeric", sigmasq="numeric", std_e= "numeric" , var_betas = "numeric",
+                                    se_betas = "numeric" ,t_values = "numeric",p_values = "numeric" ,data="data.frame",
+                                    formula="formula", dataname="character", output="data.frame"),
                       methods = list(
                         coef = function(){
                           "Method coef"
@@ -37,30 +37,40 @@ linreg <- setRefClass("linreg",
 
                           t_indep_dep<-t(indep)%*%dep #creates a matrix that is solvebal
 
-                          betahat <<- inv_sqr_dep%*%t_indep_dep # Creates betahat for the regression
+                          betahat1 <- inv_sqr_dep%*%t_indep_dep # Creates betahat for the regression
+
+                          betahat<<-as.vector(betahat1[,1])
 
                         },
                         pred = function(){
                           "Method for prediction"
                           predicted <<- indep%*%betahat
                         },
-                        resid = function(){residuals<<- predicted - dep},
+                        resid = function(){residuals<<- dep - predicted },
                         dfre = function(){ deg_fre<<- nrow(indep) - ncol(indep)  },
                         sigma= function(){   sig <- (t(residuals)%*%residuals)/deg_fre
-                        sigmasq<<- as.numeric(sig)
+                                             sig <- sig
+                                             std_e<<- as.numeric(sqrt(sig))
+                                             sigmasq<<- as.numeric(sig)
                         },
-                        vbetas=function(){  var <-sigmasq*inv_sqr_dep
-                        var_betas <<- sqrt( abs(colSums(var)) ) # Sums the variance for each colum.
+                        vbetas=function(){  var <-diag(sigmasq*inv_sqr_dep)
+                        se_betas  <<-  sqrt(var)
+                        var_betas <<-  var  # Sums the variance for each colum.
                         },
                         tvalue=function(){
                           "method t-value"
-                          t_values <<- as.numeric( betahat/ var_betas )
+                          t_values <<- as.numeric( betahat/ se_betas )
                         p_values <<- pt(t_values, deg_fre)},
                         plot  = function(){
                           "plots the residuals"
                           df<-data.frame(predicted, residuals)
                         ggplot(data = df) + aes(x =predicted, y=residuals, group=2) + geom_point()+geom_abline(intercept = 0, slope = 0 ) },
                         summary=function(){
+
+                          df<-data.frame(betahat, se_betas, t_values, p_values)
+                          rownames(df)<-colnames(indep)
+                          #df<-as.table(df)
+                          base::print(df)
 
 
 
@@ -75,6 +85,7 @@ linreg <- setRefClass("linreg",
                           "Formula for reggression"
 
                           dataname<<-deparse(substitute(data))
+                          data<<-data
                           indep<<-model.matrix(form,data)
                           dep<<-data[,all.vars(form)[1]]
 
